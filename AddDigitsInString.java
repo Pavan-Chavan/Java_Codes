d9d61043c655967cb90074403120567f6efcf094
Pavan Chavan <69139294+ig-terminal@users.noreply.github.com>
2021-06-21 17:34:55 +0530

    Update AddDigitsInString.java

--- AddDigitsInString.java
+++ AddDigitsInString.java
@@ -16,28 +16,8 @@ public class AddDigitsInString {
             s=sc.nextLine();
             for(int i=0;i<s.length();i++)
             {
-                switch(s.charAt(i))
-                {
-                    case '1':   sum=sum+1;
-                    break;
-                    case '2':   sum=sum+2;
-                    break;
-                    case '3':  sum=sum+3;
-                    break;
-                    case '4':   sum=sum+4;
-                    break;
-                    case '5':   sum=sum+5;
-                    break;
-                    case '6':   sum=sum+6;
-                    break;
-                    case '7':   sum=sum+7;
-                    break;
-                    case '8':   sum=sum+8;
-                    break;
-                    case '9':   sum=sum+9;
-                    break;
-
-                }//end of switch
+                if(Character.isDigit(s.charAt(i)))
+                sum=sum+Character.getNumericValue(s.charAt(i));
             }//end of for
         }//end of try 
         catch (Exception e) {
