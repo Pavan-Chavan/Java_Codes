d708b9ba0b651672d03cacf1724b05b47289531d
Prajakta <Prajakta@DESKTOP-44P8DBI>
2021-07-03 23:12:41 +0530

    Commited from eclipse

---
+++ JDBC_Practice/src/Oracle_Practic/PrintingTableFromOracle.java
@@ -0,0 +1,29 @@
+package Oracle_Practic;
+import java.sql.*;
+
+public class PrintingTableFromOracle {
+	public static void main(String[] args) {
+		Connection con;
+		Statement st;
+		ResultSet rs;
+		try {
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","root");
+			if (con!=null) {
+				st = con.createStatement();
+				if (st!=null) {
+					rs = st.executeQuery("select * from student");
+					while(rs.next()!=false)
+					{
+						System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+					}
+					rs.close();
+				}
+				st.close();
+			}
+			con.close();
+		}
+	catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
