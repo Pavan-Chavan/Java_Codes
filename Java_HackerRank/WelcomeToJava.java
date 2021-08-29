06095dc116cde03a7b8f0e8c5ec2284d0a575b1b
Pavan Chavan <69139294+ig-terminal@users.noreply.github.com>
2021-08-29 21:22:19 +0530

    Add files via upload

---
+++ Java_HackerRank/WelcomeToJava.java
@@ -0,0 +1,11 @@
+/* Title: Welcome to Java
+coder:Pavan Chavan
+date: 29/8/21 */
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
